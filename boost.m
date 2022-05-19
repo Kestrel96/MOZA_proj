3ddeb1c0e381feae0aaa854d84393b12b031303f
Kestrel96 <kuba.poltorak@gmail.com>
2022-05-19 23:55:30 +0200

    sasad

--- boost.m
+++ boost.m
@@ -1,7 +1,7 @@
 function bst = boost(Aac)
 %BOOST Summary of this function goes here
 %   Detailed explanation goes here
- ku0=real(Aac(1));
+ ku0=abs(Aac(1));
  ku_max=max(real(Aac));
  bst=ku_max-ku0;
 
